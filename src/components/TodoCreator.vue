e4d9e91bd23a54312f58ad951536cef907e5f2e9
aslam48 <youngfizzy001@gmail.com>
2023-02-23 10:20:52 +0100

    working

---
+++ src/components/TodoCreator.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+
+
+<template>
+ <div class="input-wrap">
+    <input type="text" />
+    <buton>Create</buton>
+ </div>
+</template>
+
+
+<style lang='scss' scoped>
+
+</style>
