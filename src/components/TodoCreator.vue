11455b887b59672b30f9d2410c58de8b0bdcc03f
aslam48 <youngfizzy001@gmail.com>
2023-02-27 06:59:17 +0100

    working

--- src/components/TodoCreator.vue
+++ src/components/TodoCreator.vue
@@ -1,15 +1,48 @@
-<script setup></script>
+<script setup>
+import {reactive, ref} from 'vue'
+
+const emit = defineEmits(['create-todo'])
+
+
+const todo = ref("")
+
+
+const createTodo = () => {
+ emit('create-todo')
+}
+</script>
 
 
 
 <template>
  <div class="input-wrap">
-    <input type="text" />
-    <buton>Create</buton>
+    <input type="text" v-model="todo" />
+    <button @click="createTodo()">Create</button>
  </div>
+    <p>{{todo}}</p>
 </template>
 
 
 <style lang='scss' scoped>
-
+.input-wrap {
+  display: flex;
+  transition: 250ms ease;
+  border: 2px solid #41b080;
+  &:focus-within {
+    box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
+      0 -2px 4px -2px rgb(0 0 0 / 0.1);
+  }
+  input {
+    width: 100%;
+    padding: 8px 6px;
+    border: none;
+    &:focus {
+      outline: none;
+    }
+  }
+  button {
+    padding: 8px 16px;
+    border: none;
+  }
+}
 </style>
